1403c2744ebffb4ca5e25c6a513b5a43b5098aba
Fatma Ashraf <fatma_ashraf12@yahoo.com>
2022-11-17 16:31:23 +0200

    install eslint , vite , prettier for react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>please wait</h1>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<App />);
